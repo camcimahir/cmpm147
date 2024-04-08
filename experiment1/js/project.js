77ec1646f72aada3dedde69a7dcb7e1f1f6bd943
Mahir <camcimahir@gmail.com>
2024-04-07 17:03:00 -0700

    final

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -24,11 +24,53 @@ class MyProjectClass {
 
 function main() {
   // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
+  const fillers = {
+    protagonist: ["The Emporer", "my child", "THIEF", "Soldier", "Old man", "No One", "Professor", "Chosen One"],
+    places: ["Demacia", "The United States of America", "land far far away", "the spirit world", "your dreams", "your past", "your future", "Planet Mars", "the ministry of defense representing our universe"],
+    land: ["Demacia", "The United States of America", "land far far away", "the spirit world", "your dreams", "your past", "your future", "Planet Mars", "Santa Cruz", "The Universe", "The World", "UCSC", "your friend"],
+    quest: ["save the slugs", "defeat the dark lord", "break the curse", "Fix the housing prices", "restore peace", "graduate college", "convince my father", "find the legendary treasure", "find the last airbender"],
+    reward: ["1000 dollars", "trip to Six Flags", "2012 Honda Civic", "riches beyond imgination", "eternal youth", "ultimate knowledge", "unlimited wishes", "free housing in Santa Cruz for a year", "the heart's deepest desire"],
+  };
+  
+  const template = `Hello there, $protagonist!
+  
+  I come from $places. There are huge catastrophes awaiting $land, and it is your faith and yours only to embark on this ques to $quest. 
+  
+  Ofcourse success of your mission will result in the best reward anyone could ask for: $reward.
+  `;
+  
+  // STUDENTS: You don't need to edit code below this line.
+  
+  const slotPattern = /\$(\w+)/;
+  
+  function replacer(match, name) {
+    let options = fillers[name];
+    if (options) {
+      return options[Math.floor(Math.random() * options.length)];
+    } else {
+      return `<UNKNOWN:${name}>`;
+    }
+  }
+  
+  function generate() {
+    let story = template;
+    while (story.match(slotPattern)) {
+      story = story.replace(slotPattern, replacer);
+    }
+  
+    /* global box */
+    $("#box").text(story);
+  }
+  
+  /* global clicker */
+  $("#clicker").click(generate);
+  
+  generate();
+  
 
   // call a method on the instance
   myInstance.myMethod();
 }
 
 // let's get this party started - uncomment me
-//main();
+main();
